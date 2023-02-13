566d19b0df5cd22be832afe60e89200860610f44
Alex <42418433+aiexz@users.noreply.github.com>
2023-02-13 18:47:41 +0300

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import datetime
+import time
+import os
+
+if not os.path.exists('docs'):
+    os.mkdir('docs')
+
+
+with open('docs/index.html', 'w') as f:
+    with open('template/index.html', 'r') as r:
+        content = r.read()
+        content = content.format(action_time=datetime.datetime.now())
+        f.write(content)
